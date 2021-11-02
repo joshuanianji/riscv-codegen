1d677d7d2bab8744b13da6373977b93929360af1
Joshua Ji <joshua.ji@jiwhiz.com>
2021-11-02 00:56:05 -0600

    Initialize project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <GeistProvider>
+    <CssBaseline /> {/* Normalize CSS */}
+    <Component {...pageProps} />
+  </GeistProvider>
+)
 
 export default MyApp
